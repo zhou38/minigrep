596ad661ee8e17e80a6080a677756a643a625dd7
zhouziyu <463098920@qq.com>
2024-08-05 17:24:15 +0800

    错误处理

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -19,9 +23,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config{
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Ok(Config { query, file_path })
+    }
 }
